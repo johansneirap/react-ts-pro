2f3144c649118f63bb0fd4bdda4feb22010a4288
johansneirap <johansneirap@gmail.com>
2022-03-04 22:31:27 -0300

    firstCommit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Navigation } from './routes/Navigation';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navigation />
+    </>
+  )
+}
+
+export default App
