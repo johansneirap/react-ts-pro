908b3c7389e7ff0662117207f46340c1aee38ca4
johansneirap <johansneirap@gmail.com>
2022-03-15 22:41:46 -0300

    pre npm ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
 import { ProductCard, ProductImage, ProductTitle, ProductButtonsCounter } from "../components";
 import { products } from "../data/products";
-import { Product } from "../interfaces/productInterfaces";
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -15,7 +12,6 @@ export const ShoppingPage = () => {
         <ProductCard
           key={ product.id }
           product={ product }
-          className="bg-dark text-white"
           initialValues={{
             count: 4,
             maxCount: 10,
@@ -24,13 +20,9 @@ export const ShoppingPage = () => {
           {
             ({ reset, increaseBy, count }) => (
               <>
-                <ProductCard.Image className="custom-image" />
-                <ProductCard.Title className="text-white text-bold" />
-                <ProductCard.Buttons className="custom-buttons" />
-                <button onClick={reset}>Reset</button>
-                <button onClick={ ()=> increaseBy( -2 )}> -2 </button>
-                <button onClick={ ()=> increaseBy( +2 )}> +2 </button>
-                <span>{ count }</span>
+                <ProductCard.Image />
+                <ProductCard.Title />
+                <ProductCard.Buttons />
               </>
             )
           }
